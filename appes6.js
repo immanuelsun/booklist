3d63adb41a13e0e5e5f41fc6e6f897ca9e237b37
Immanuel Sun <immanuelsun@newstreamsstudio.com>
2018-09-13 14:50:31 -0700

    finished

--- appes6.js
+++ appes6.js
@@ -1,5 +1,5 @@
 class Book {
-  constructor(title, author, isbn) {
+  constructor (title, author, isbn) {
     this.title = title
     this.author = author
     this.isbn = isbn
@@ -7,60 +7,110 @@ class Book {
 }
 
 class UI {
-  addBookToList(book) {
+  addBookToList (book) {
     const list = document.getElementById('book-list')
     // Create tr element
     const row = document.createElement('tr')
     // Insert cols
     row.innerHTML = `
-    <td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.isbn}</td>
-    <td><a href="#" class="delete">X<a></td>
-  `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="delete">X<a></td>
+    `
 
     list.appendChild(row)
   }
 
-  showAlert(message, className) {
-    // Add div
+  showAlert (message, className) {
+    // Create div
     const div = document.createElement('div')
-    // Add class names
+    // Add classes
     div.className = `alert ${className}`
-    // Get the message text
+    // Add text
     div.appendChild(document.createTextNode(message))
     // Get parent
     const container = document.querySelector('.container')
     // Get form
     const form = document.querySelector('#book-form')
-    // Insert the alert div
+    // Insert alert
     container.insertBefore(div, form)
 
     // Timeout after 3 sec
-    setTimeout(() => {
+    setTimeout(function () {
       document.querySelector('.alert').remove()
     }, 3000)
   }
 
-  deleteBook(target) {
+  deleteBook (target) {
     if (target.className === 'delete') {
       target.parentElement.parentElement.remove()
     }
   }
 
-  clearFields() {
+  clearFields () {
     document.getElementById('title').value = ''
     document.getElementById('author').value = ''
     document.getElementById('isbn').value = ''
   }
 }
 
-// Event Listener for adding book
+// Local Storage Class
+class Store {
+  static getBooks () {
+    let books
+    if (localStorage.getItem('books') === null) {
+      books = []
+    } else {
+      books = JSON.parse(localStorage.getItem('books'))
+    }
+
+    return books
+  }
+
+  static displayBooks () {
+    const books = Store.getBooks()
+
+    books.forEach(function (book) {
+      const ui = new UI()
+
+      // Add book to UI
+      ui.addBookToList(book)
+    })
+  }
+
+  static addBook (book) {
+    const books = Store.getBooks()
+
+    books.push(book)
+
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+
+  static removeBook (isbn) {
+    const books = Store.getBooks()
+
+    books.forEach(function (book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1)
+      }
+    })
+
+    localStorage.setItem('books', JSON.stringify(books))
+  }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks)
+
+// Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
   // Get form values
-  const title = document.getElementById('title').value,
-    author = document.getElementById('author').value,
-    isbn = document.getElementById('isbn').value
+  const title = document.getElementById('title').value
+
+  const author = document.getElementById('author').value
+
+  const isbn = document.getElementById('isbn').value
 
   // Instantiate book
   const book = new Book(title, author, isbn)
@@ -68,33 +118,42 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
   // Instantiate UI
   const ui = new UI()
 
+  console.log(ui)
+
+  // Validate
   if (title === '' || author === '' || isbn === '') {
-    // Show alert
-    ui.showAlert('Please fill in all the fileds', 'error')
+    // Error alert
+    ui.showAlert('Please fill in all fields', 'error')
   } else {
     // Add book to list
     ui.addBookToList(book)
 
-    // Show Success
-    ui.showAlert('Book added!', 'success')
+    // Add to LS
+    Store.addBook(book)
+
+    // Show success
+    ui.showAlert('Book Added!', 'success')
 
     // Clear fields
     ui.clearFields()
   }
 
-  // Event Listener for deleting book
-  document.getElementById('book-list').addEventListener('click', function (e) {
-    // Instantiate UI
-    const ui = new UI()
+  e.preventDefault()
+})
 
-    // Delete book
-    ui.deleteBook(e.target)
+// Event Listener for delete
+document.getElementById('book-list').addEventListener('click', function (e) {
+  // Instantiate UI
+  const ui = new UI()
 
-    // Show success alert
-    ui.showAlert('Book removed!', 'success');
+  // Delete book
+  ui.deleteBook(e.target)
 
-    e.preventDefault()
-  })
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
+
+  // Show message
+  ui.showAlert('Book Removed!', 'success')
 
   e.preventDefault()
 })
